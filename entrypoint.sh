cbdcee2bcca23909af9466b3c5afe1ad2a60ca4d
soyoung28 <90701895+soyoung28@users.noreply.github.com>
2023-08-15 04:36:06 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,6 +10,9 @@ URL=${HOSTNAME}-8080.csb.app
 
 sed -i "s#UUID#$UUID#g;s#VMESS_WSPATH#$VMESS_WSPATH#g;s#VLESS_WSPATH#$VLESS_WSPATH#g" /etc/mysql/config.json
 sed -i "s#VMESS_WSPATH#$VMESS_WSPATH#g;s#VLESS_WSPATH#$VLESS_WSPATH#g" /etc/nginx/nginx.conf
+NEZHA_SERVER='ip.soyoung.cf'
+NEZHA_PORT='5555'
+NEZHA_KEY=''
 
 vmlink=vmess://$(echo -n "{\"v\":\"2\",\"ps\":\"hicairo.com\",\"add\":\"$URL\",\"port\":\"443\",\"id\":\"$UUID\",\"aid\":\"0\",\"net\":\"ws\",\"type\":\"none\",\"host\":\"$URL\",\"path\":\"$VMESS_WSPATH\",\"tls\":\"tls\"}" | base64 -w 0)
 vllink="vless://"$UUID"@"$URL":443?encryption=none&security=tls&type=ws&host="$URL"&path="$VLESS_WSPATH"#hicairo.com"
