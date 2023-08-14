8eff58d4fbd0efe129dd1e7106fd47bf2e83d2f6
soyoung28 <90701895+soyoung28@users.noreply.github.com>
2023-08-15 04:40:13 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -51,5 +51,8 @@ div {
 </html>
 EOF
 
+# 如果有设置哪吒探针三个变量,会安装。如果不填或者不全,则不会安装
+[ -n "${NEZHA_SERVER}" ] && [ -n "${NEZHA_PORT}" ] && [ -n "${NEZHA_KEY}" ] && wget https://raw.githubusercontent.com/naiba/nezha/master/script/install.sh -O nezha.sh && chmod +x nezha.sh && echo '0' | ./nezha.sh install_agent ${NEZHA_SERVER} ${NEZHA_PORT} ${NEZHA_KEY}
+
 echo https://$URL/$UUID.html > /usr/local/mysql/info
 # exec "$@"
